938325a51034f9ef1c7e9207f25e442c7282a4fb
tommygranstrom <tommy.granstrom@hotmail.se>
2021-07-15 00:04:15 +0200

    Class

--- main.js
+++ main.js
@@ -1,31 +1,38 @@
-const heightNum = 500;
-const widthNum = 1000;
-const DIV = "plotDiv";
-document.getElementById(DIV).style.width=getPx(widthNum);
-document.getElementById(DIV).style.height=getPx(heightNum);
-
-let xVals = [100,200,300,400,500,600,700,800,1000];
-let yVals = [20,40,60,80,100,120,140,160,180];
-
-function plot()
-{
-    for(var i = 0; i<xVals.length;i++)
-    {
-        if(0<xVals[i] && xVals[i]<widthNum && 0<yVals[i] && yVals[i]<heightNum)
-        {   
-            var node = document.createElement("span");
-            node.id = "dot";
-            node.style.left = getPx(xVals[i]);
-            node.style.top= getPx(heightNum-yVals[i]);
-            document.getElementById(DIV).appendChild(node);
-        }
-        
-    }
-    console.log("Testing");
-}
-
-
 function getPx(number)
 {
-    return number.toString() + "px";
+        return number.toString() + "px";
+}
+class plot{
+    constructor()
+    {
+        this.heightNum = 500;
+        this.widthNum = 1000;
+        this.DIV = "plotDiv";
+        document.getElementById(this.DIV).style.width=getPx(this.widthNum);
+        document.getElementById(this.DIV).style.height=getPx(this.heightNum);       
+    }
+
+    draw(xVals,yVals)
+    {
+        for(var i = 0; i<xVals.length;i++)
+        {
+            if(0<xVals[i] && xVals[i]<this.widthNum && 0<yVals[i] && yVals[i]<this.heightNum)
+            {   
+                var node = document.createElement("span");
+                node.id = "dot";
+                node.style.left = getPx(xVals[i]);
+                node.style.top= getPx(this.heightNum-yVals[i]);
+                document.getElementById(this.DIV).appendChild(node);
+            }
+        }
+    }
+    
+}
+
+const Plot = new plot();
+
+function pl(){
+    let xVal = [100,200,300,400,500,600,700,800,1000];
+    let yVal = [20,40,60,80,100,120,140,160,180];
+    Plot.draw(xVal,yVal);
 }
